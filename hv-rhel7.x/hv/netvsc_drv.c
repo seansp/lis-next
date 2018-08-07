a9cf597800810e40071e91a84e134516abd84af7
Sean Spratt <seansp@gmail.com>
2018-08-07 13:58:38 -0700

    RH7: scsi: netvsc: Use the vmbus function to calculate ring buffer percentage

--- hv-rhel7.x/hv/netvsc_drv.c
+++ hv-rhel7.x/hv/netvsc_drv.c
@@ -35,8 +35,6 @@
 #include <linux/slab.h>
 #include <linux/rtnetlink.h>
 #include <linux/netpoll.h>
-#include <linux/reciprocal_div.h>
-
 #include <net/arp.h>
 #include <net/route.h>
 #include <net/sock.h>
@@ -46,6 +44,10 @@
 #include <net/ip6_checksum.h>
 
 #include "hyperv_net.h"
+
+#if (RHEL_RELEASE_CODE == RHEL_RELEASE_VERSION(7,0))
+#include <linux/reciprocal_div.h>
+#endif
 
 #define RING_SIZE_MIN	64
 #define RETRY_US_LO	5000
@@ -62,8 +64,6 @@ unsigned int netvsc_ring_bytes;
 
 #if (RHEL_RELEASE_CODE == RHEL_RELEASE_VERSION(7,0))
 u32 netvsc_ring_reciprocal;
-#else
-struct reciprocal_value netvsc_ring_reciprocal;
 #endif
 
 static const u32 default_msg = NETIF_MSG_DRV | NETIF_MSG_PROBE |
@@ -2229,7 +2229,9 @@ static int __init netvsc_drv_init(void)
 			ring_size);
 	}
 	netvsc_ring_bytes = ring_size * PAGE_SIZE;
+#if (RHEL_RELEASE_CODE == RHEL_RELEASE_VERSION(7,0)) 
 	netvsc_ring_reciprocal = reciprocal_value(netvsc_ring_bytes);
+#endif
 
 	ret = vmbus_driver_register(&netvsc_drv);
 	if (ret)
